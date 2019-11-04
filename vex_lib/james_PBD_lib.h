8f52b174c5659cdd21e49f5aaa064f94aef6c94b
visualoncloude <38007938+visualoncloude@users.noreply.github.com>
2019-11-04 18:39:10 -0500

    Update james_PBD_lib.h

--- vex_lib/james_PBD_lib.h
+++ vex_lib/james_PBD_lib.h
@@ -1,3 +1,42 @@
+﻿
+void computeMatrixK( vector connector;
+	         float invMass;
+	         vector x;
+	         matrix3 inertiaInverseW;
+	         matrix3 K)
+{
+	if (invMass != 0.0)
+	{
+		vector v = connector - x;
+		float  a = v[0];
+		float b = v[1];
+		float c = v[2];
+
+		// J is symmetric
+		float j11 = inertiaInverseW.xx;
+		float j12 = inertiaInverseW.xy;
+		float j13 = inertiaInverseW.xz;
+		float j22 = inertiaInverseW.yy;
+		float j23 = inertiaInverseW.yz;
+		float j33 = inertiaInverseW.zz;
+
+		K.xx = c*c*j22 - b*c*(j23 + j23) + b*b*j33 + invMass;
+		K.xy = -(c*c*j12) + a*c*j23 + b*c*j13 - a*b*j33;
+		K.xz = b*c*j12 - a*c*j22 - b*b*j13 + a*b*j23;
+		K.yx = K.xy;
+		K.yy = c*c*j11 - a*c*(j13 + j13) + a*a*j33 + invMass;
+		K.yz = -(b*c*j11) + a*c*j12 + a*b*j13 - a*a*j23;
+		K.zx = K.xz;
+		K.zy = K.yz;
+		K.zz = b*b*j11 - a*b*(j12 + j12) + a*a*j22 + invMass;
+	}
+	else
+	{
+		K = set(0,0,0,0,0,0,0,0,0);
+	}
+}
+
+
 
 
 vector center_edge(vector p0 , p1)
@@ -11,52 +50,30 @@ void init_BallJoint(vector  x0 , x1 ;
                     vector ballJointPosition;
                     vector ballJointInfo[])
                     {
-                        matrix3 rot0T = matrix3_transpose(qconvert(q0));
-                        matrix3 rot1T = matrix3_transpose(qconvert(q1));
-                        ballJointInfo[0]= rot0T * (ballJointPosition - x0);
+			matrix3 q0_m =  qconvert(q0);
+			matrix3 q1_m =  qconvert(q1);                       
+			matrix3 rot0T = matrix3_transpose(q0_m);
+                        matrix3 rot1T = matrix3_transpose(q1_m);
+                        ballJointInfo[0] = rot0T * (ballJointPosition - x0);
                         ballJointInfo[1] = rot1T * (ballJointPosition - x1);
-                        ballJointInfo[2] = ballJointPosition;
-                        ballJointInfo[3] = ballJointPosition;
+                        ballJointInfo[2] = ballJointPosition-  ballJointInfo[0];
+                        ballJointInfo[3] = ballJointPosition- ballJointInfo[1];
                     }
 
 
 void update_BallJoint(vector  x0 , x1 ;
                     vector4 q0, q1 ;
-                    vector ballJointPosition;
                     vector ballJointInfo[])
                     {
-                        matrix3 rot0T = matrix3_transpose(qconvert(q0));
-                        matrix3 rot1T = matrix3_transpose(qconvert(q1));
-                        ballJointInfo[2]= rot0T * (ballJointPosition[0] - x0)  + x0;
-                        ballJointInfo[3] = rot1T * (ballJointPosition[1] - x1) + x1;
-                       
+			//matrix3 q0_m =  qconvert(q0);
+			//matrix3 q1_m =  qconvert(q1);                         
+			matrix3 rot0 = qconvert(q0);
+                        matrix3 rot1 =  qconvert(q1); 
+			ballJointInfo[2]= rot0 * ballJointInfo[0]  + x0;
+			ballJointInfo[3] = rot1 * ballJointInfo[1]  + x1;
+
                     }
 
-
-
-void DistanceConstraint( vector  p0, p1 ; 
-                    float invMass0, invMass1;
-                    float  restLength;
-                    float compressionStiffness, stretchStiffness;
-                    vector corr0, corr1)
-        {                               
-                float wSum = invMass0 + invMass1;
-                vector n = p1 - p0;
-                float d = length(n);
-                 n  = normalize(n);
-                
-                vector corr;
-                if (d < restLength)
-                        corr = compressionStiffness * n * (d - restLength) / wSum;
-                else
-                        corr = stretchStiffness * n * (d - restLength) / wSum;
-        
-                corr0 =  invMass0 * corr;
-                corr1 = -invMass1 * corr;
-                
-        }
-        
-/*
 void solve_BallJoint(vector  x0 , x1 ;
 		     float  invMass0 , invMass1;
 		     vector4 q0, q1 ;
@@ -93,5 +110,17 @@ void solve_BallJoint(vector  x0 , x1 ;
                      
 		     
 		     }
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
